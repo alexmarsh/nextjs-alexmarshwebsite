27f41792545f5033d36b636543698d227b003811
Alex Marsh <marsh.alex@outlook.com>
2025-11-12 05:26:13 -0500

    feat: Add TechItem component and technologies data

---
+++ src/data/technologies.ts
@@ -0,0 +1,96 @@
+// src/data/technologies.ts
+
+export const technologies = [
+    {
+        title: 'Adobe Illustrator',
+        icon: 'icon-adobe-illustrator'
+    },
+    {
+        title: 'Adobe Photoshop',
+        icon: 'icon-adobe-photoshop'
+    },
+    {
+        title: 'Bootstrap',
+        icon: 'icon-bootstrap'
+    },
+    {
+        title: 'CSS3',
+        icon: 'icon-css3'
+    },
+    {
+        title: 'D3.js',
+        icon: 'icon-d3'
+    },
+    {
+        title: 'Git',
+        icon: 'icon-git'
+    },
+    {
+        title: 'Google Analytics',
+        icon: 'icon-google-analytics'
+    },
+    {
+        title: 'Google Calendar API',
+        icon: 'icon-google-calendar'
+    },
+    {
+        title: 'Google Maps API',
+        icon: 'icon-google-maps'
+    },
+    {
+        title: 'HTML5',
+        icon: 'icon-html5'
+    },
+    {
+        title: 'Instagram API',
+        icon: 'icon-instagram'
+    },
+    {
+        title: 'JavaScript',
+        icon: 'icon-javascript'
+    },
+    {
+        title: 'jQuery',
+        icon: 'icon-jquery'
+    },
+    {
+        title: 'JSP / JSTL',
+        icon: 'icon-jsp'
+    },
+    {
+        title: 'PHP',
+        icon: 'icon-php'
+    },
+    {
+        title: 'Progressive Web Apps',
+        icon: 'icon-pwa'
+    },
+    {
+        title: 'Responsive Design',
+        icon: 'icon-responsive-design'
+    },
+    {
+        title: 'SoundCloud API',
+        icon: 'icon-soundcloud'
+    },
+    {
+        title: 'SQL / PostgreSQL',
+        icon: 'icon-sql'
+    },
+    {
+        title: 'SVG',
+        icon: 'icon-svg'
+    },
+    {
+        title: 'Twitter API',
+        icon: 'icon-twitter'
+    },
+    {
+        title: 'UI/UX',
+        icon: 'icon-ui'
+    },
+    {
+        title: 'WordPress',
+        icon: 'icon-wordpress'
+    }
+]
