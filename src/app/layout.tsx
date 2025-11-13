8ffea2ba0f5ccde9b17cebb9a191690a75896ebc
Alex Marsh <marsh.alex@outlook.com>
2025-11-13 13:42:46 -0500

    feat: Add logo.svg for favicon and shortcut icons in layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,11 @@ const openSans = Open_Sans({
 export const metadata: Metadata = {
     title: "Alex Marsh - Professional Web Developer based in Buffalo, New York",
     description: "Let me help you build your next website. With over a decade of experience building websites for a wide range of industries, including banking, legal, medical and more, I have a trusted background that can help you take your business to the next level.",
+    icons: {
+        icon: "/img/logo.svg", // used for favicon
+        shortcut: "/img/logo.svg", // used for shortcut icon (optional)
+        apple: "/img/logo.svg", // used for iOS
+    },
 };
 
 export default async function RootLayout({
