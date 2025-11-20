0d25628a2b90e9e2986e8ece9cb747be19db1a51
Alex Marsh <marsh.alex@outlook.com>
2025-11-20 04:09:52 -0500

    update screenshot image size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,8 +46,8 @@ export const metadata: Metadata = {
         images: [
             {
                 url: "/img/screenshot.png",
-                width: 800,
-                height: 600,
+                width: 1200,
+                height: 630,
                 alt: "Alex Marsh Website Preview",
             },
         ],
