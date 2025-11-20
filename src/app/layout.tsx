43c1dc59598161a52997bca1a51d0e5e4942b266
Alex Marsh <marsh.alex@outlook.com>
2025-11-20 04:00:02 -0500

    update: add canonical & opengraph tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "../styles/_global.scss";
-
 import { Kaushan_Script, Open_Sans } from "next/font/google";
 
 const kaushanScript = Kaushan_Script({
@@ -22,18 +21,45 @@ const openSans = Open_Sans({
 });
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://alexmarshwebsite.com"),
     title: "Alex Marsh - Professional Web Developer based in Buffalo, New York",
     description: "Let me help you build your next website. With over a decade of experience building websites for a wide range of industries, including banking, legal, medical and more, I have a trusted background that can help you take your business to the next level.",
+    alternates: {
+        canonical: "./",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-snippet': -1,
+            'max-image-preview': "large",
+            'max-video-preview': -1,
+        },
+    },
+    openGraph: {
+        type: "website",
+        url: "https://alexmarshwebsite.com",
+        title: "Alex Marsh, Professional Web Developer",
+        siteName: "Alex Marsh Website",
+        images: [
+            {
+                url: "/img/screenshot.png",
+                width: 800,
+                height: 600,
+                alt: "Alex Marsh Website Preview",
+            },
+        ],
+    },
     icons: {
-        icon: "/img/logo.svg", // used for favicon
-        shortcut: "/img/logo.svg", // used for shortcut icon (optional)
-        apple: "/img/logo.svg", // used for iOS
+        icon: "/img/logo.svg",
+        shortcut: "/img/logo.svg",
+        apple: "/img/logo.svg",
     },
 };
 
-export default async function RootLayout({
-    children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className={`${openSans.variable} ${kaushanScript.variable}`}>
             <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
